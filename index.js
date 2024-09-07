ab4631cccd20745642922dfc5403c29ca233a44f
devsingh1234 <divyanshksingh9451@gmail.com>
2024-09-07 13:33:56 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// var bulb = document.querySelector("#bulb")
+// var button = document.querySelector("button")
+// var flag = 0
+
+// button.addEventListener("click",function(){
+// if (flag == 0){
+// bulb.style.backgroundColor = "black"
+// button.innerHTML = "ON"
+// flag = 1
+
+// }
+// else{
+//     bulb.style.backgroundColor = "yellow"
+//     button.innerHTML = "OFF"
+//     flag = 0
+// }
+
+// })
